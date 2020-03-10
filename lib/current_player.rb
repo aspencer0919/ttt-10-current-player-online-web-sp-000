6e82510cbff17b96525c69c69e2384d3bd688bc7
Adam Spencer <aspencer0919@gmail.com>
2020-03-10 16:42:13 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+counter = 0
+
+def turn_count(board)
+  counter = 0
+  board.count do |space|
+    (space.include? " ")
+    counter = 9-space
+  end
+end
+
+def current_player(counter)
+  if counter.even?
+    puts "O"
+  else
+    puts "X"
+  end
+end
